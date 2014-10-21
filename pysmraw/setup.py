de8afdb8c993f3880aff773199651908318617c0
Joachim Metz <joachim.metz@gmail.com>
2014-10-21 22:53:16 +0200

    Changes for deployment.

--- pysmraw/setup.py
+++ pysmraw/setup.py
@@ -26,10 +26,10 @@ class custom_sdist(sdist):
 
 
 LIBRARY_NAME = "libsmraw"
-LIBRARY_VERSION = "20141007"
+LIBRARY_VERSION = "20141021"
 MODULE_NAME = "py%s" % (LIBRARY_NAME[3:])
 # bdist_msi does not support the library version, neither a date as a version.
-MODULE_VERSION = "20141007.1"
+MODULE_VERSION = "20141021.1"
 PROJECT_URL = "https://github.com/libyal/%s/" % (LIBRARY_NAME)
 
 DIRECTORY_NAMES = [
