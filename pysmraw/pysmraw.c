e15ffb34a9e8dd5fb53ea350f0e2a0316f899a09
Joachim Metz <joachim.metz@gmail.com>
2014-10-30 06:15:25 +0100

    Bug fix in Python-bindings.

--- pysmraw/pysmraw.c
+++ pysmraw/pysmraw.c
@@ -102,9 +102,7 @@ PyObject *pysmraw_get_version(
 	         errors ) );
 }
 
-#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
-
-/* Globs filenames according to the Expert Witness Compression Format (SMRAW) segment file naming schema
+/* Globs filenames according to the storage media (SM) (split) RAW segment file naming schema
  * Returns a Python object if successful or NULL on error
  */
 PyObject *pysmraw_glob(
@@ -112,7 +110,6 @@ PyObject *pysmraw_glob(
            PyObject *arguments,
            PyObject *keywords )
 {
-	wchar_t **filenames_wide         = NULL;
 	char **filenames_narrow          = NULL;
 	libcerror_error_t *error         = NULL;
 	PyObject *exception_string       = NULL;
@@ -122,7 +119,6 @@ PyObject *pysmraw_glob(
 	PyObject *filename_string_object = NULL;
 	PyObject *list_object            = NULL;
 	PyObject *string_object          = NULL;
-	const wchar_t *filename_wide     = NULL;
 	static char *function            = "pysmraw_glob";
 	static char *keyword_list[]      = { "filename", NULL };
 	const char *errors               = NULL;
@@ -133,12 +129,19 @@ PyObject *pysmraw_glob(
 	int number_of_filenames          = 0;
 	int result                       = 0;
 
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
+	wchar_t **filenames_wide         = NULL;
+	const wchar_t *filename_wide     = NULL;
+#else
+	PyObject *utf8_string_object     = NULL;
+#endif
+
 	PYSMRAW_UNREFERENCED_PARAMETER( self )
 
 	/* Note that PyArg_ParseTupleAndKeywords with "s" will force Unicode strings to be converted to narrow character string.
 	 * On Windows the narrow character strings contains an extended ASCII string with a codepage. Hence we get a conversion
-	 * exception. We cannot use "u" here either since that does not allow us to pass non Unicode string objects and
-	 * Python (at least 2.7) does not seems to automatically upcast them.
+	 * exception. This will also fail if the default encoding is not set correctly. We cannot use "u" here either since that
+	 * does not allow us to pass non Unicode string objects and Python (at least 2.7) does not seems to automatically upcast them.
 	 */
 	if( PyArg_ParseTupleAndKeywords(
 	     arguments,
@@ -192,6 +195,7 @@ PyObject *pysmraw_glob(
 	{
 		PyErr_Clear();
 
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
 		filename_wide = (wchar_t *) PyUnicode_AsUnicode(
 		                             string_object );
 
@@ -208,7 +212,60 @@ PyObject *pysmraw_glob(
 			  &error );
 
 		Py_END_ALLOW_THREADS
+#else
+		utf8_string_object = PyUnicode_AsUTF8String(
+		                      string_object );
 
+		if( utf8_string_object == NULL )
+		{
+			PyErr_Fetch(
+			 &exception_type,
+			 &exception_value,
+			 &exception_traceback );
+
+			exception_string = PyObject_Repr(
+					    exception_value );
+
+			error_string = PyString_AsString(
+					exception_string );
+
+			if( error_string != NULL )
+			{
+				PyErr_Format(
+				 PyExc_RuntimeError,
+				 "%s: unable to convert unicode string to UTF-8 with error: %s.",
+				 function,
+				 error_string );
+			}
+			else
+			{
+				PyErr_Format(
+				 PyExc_RuntimeError,
+				 "%s: unable to convert unicode string to UTF-8.",
+				 function );
+			}
+			Py_DecRef(
+			 exception_string );
+
+			return( NULL );
+		}
+		filename_narrow = PyString_AsString(
+				   utf8_string_object );
+
+		Py_BEGIN_ALLOW_THREADS
+
+		result = libsmraw_glob(
+			  filename_narrow,
+			  filename_length,
+			  &filenames_narrow,
+			  &number_of_filenames,
+			  &error );
+
+		Py_END_ALLOW_THREADS
+
+		Py_DecRef(
+		 utf8_string_object );
+#endif
 		if( result != 1 )
 		{
 			pysmraw_error_raise(
@@ -229,13 +286,26 @@ PyObject *pysmraw_glob(
 		     filename_index < number_of_filenames;
 		     filename_index++ )
 		{
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
 			filename_length = libcstring_wide_string_length(
 					   filenames_wide[ filename_index ] );
 
 			filename_string_object = PyUnicode_FromWideChar(
 						  filenames_wide[ filename_index ],
 						  filename_length );
+#else
+			filename_length = libcstring_narrow_string_length(
+			                   filenames_narrow[ filename_index ] );
 
+			/* Pass the string length to PyUnicode_DecodeUTF8
+			 * otherwise it makes the end of string character is part
+			 * of the string
+			 */
+			filename_string_object = PyUnicode_DecodeUTF8(
+			                          filenames_narrow[ filename_index ],
+			                          filename_length,
+			                          errors );
+#endif
 			if( filename_string_object == NULL )
 			{
 				PyErr_Format(
@@ -263,10 +333,17 @@ PyObject *pysmraw_glob(
 		}
 		Py_BEGIN_ALLOW_THREADS
 
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
 		result = libsmraw_glob_wide_free(
 			  filenames_wide,
 			  number_of_filenames,
 			  &error );
+#else
+		result = libsmraw_glob_free(
+			  filenames_narrow,
+			  number_of_filenames,
+			  &error );
+#endif
 
 		Py_END_ALLOW_THREADS
 
@@ -450,6 +527,7 @@ on_error:
 
 		Py_END_ALLOW_THREADS
 	}
+#if defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER )
 	if( filenames_wide != NULL )
 	{
 		Py_BEGIN_ALLOW_THREADS
@@ -461,167 +539,9 @@ on_error:
 
 		Py_END_ALLOW_THREADS
 	}
+#endif
 	return( NULL );
 }
-
-#else
-
-/* Globs filenames according to the Expert Witness Compression Format (SMRAW) segment file naming schema
- * Returns a Python object if successful or NULL on error
- */
-PyObject *pysmraw_glob(
-           PyObject *self PYSMRAW_ATTRIBUTE_UNUSED,
-           PyObject *arguments,
-           PyObject *keywords )
-{
-	char **filenames                 = NULL;
-	libcerror_error_t *error         = NULL;
-	PyObject *filename_string_object = NULL;
-	PyObject *list_object            = NULL;
-	static char *function            = "pysmraw_glob";
-	static char *keyword_list[]      = { "filename", NULL };
-	const char *errors               = NULL;
-	const char *filename             = NULL;
-	size_t filename_length           = 0;
-	int filename_index               = 0;
-	int number_of_filenames          = 0;
-	int result                       = 0;
-
-	PYSMRAW_UNREFERENCED_PARAMETER( self )
-
-	/* Note that PyArg_ParseTupleAndKeywords with "s" will force Unicode strings to be converted to narrow character string.
-	 * For systems that support UTF-8 this works for Unicode string objects as well.
-	 */
-	if( PyArg_ParseTupleAndKeywords(
-	     arguments,
-	     keywords,
-	     "|s",
-	     keyword_list,
-	     &filename ) == 0 )
-	{
-		return( NULL );
-	}
-	filename_length = libcstring_narrow_string_length(
-	                   filename );
-
-	Py_BEGIN_ALLOW_THREADS
-
-	result = libsmraw_glob(
-	          filename,
-	          filename_length,
-	          &filenames,
-	          &number_of_filenames,
-	          &error );
-
-	Py_END_ALLOW_THREADS
-
-	if( result != 1 )
-	{
-		pysmraw_error_raise(
-		 error,
-		 PyExc_IOError,
-		 "%s: unable to glob filenames.",
-		 function );
-
-		libcerror_error_free(
-		 &error );
-
-		goto on_error;
-	}
-	list_object = PyList_New(
-	               (Py_ssize_t) number_of_filenames );
-
-	for( filename_index = 0;
-	     filename_index < number_of_filenames;
-	     filename_index++ )
-	{
-		filename_length = libcstring_narrow_string_length(
-		                   filenames[ filename_index ] );
-
-		/* Pass the string length to PyUnicode_DecodeUTF8
-		 * otherwise it makes the end of string character is part
-		 * of the string
-		 */
-		filename_string_object = PyUnicode_DecodeUTF8(
-		                          filenames[ filename_index ],
-		                          filename_length,
-		                          errors );
-
-		if( filename_string_object == NULL )
-		{
-			PyErr_Format(
-			 PyExc_IOError,
-			 "%s: unable to convert UTF-8 filename: %d into Unicode.",
-			 function,
-			 filename_index );
-
-			goto on_error;
-		}
-		if( PyList_SetItem(
-		     list_object,
-		     (Py_ssize_t) filename_index,
-		     filename_string_object ) != 0 )
-		{
-			PyErr_Format(
-			 PyExc_MemoryError,
-			 "%s: unable to set filename: %d in list.",
-			 function,
-			 filename_index );
-
-			goto on_error;
-		}
-		filename_string_object = NULL;
-	}
-	Py_BEGIN_ALLOW_THREADS
-
-	result = libsmraw_glob_free(
-	          filenames,
-	          number_of_filenames,
-	          &error );
-
-	Py_END_ALLOW_THREADS
-
-	if( result != 1 )
-	{
-		pysmraw_error_raise(
-		 error,
-		 PyExc_MemoryError,
-		 "%s: unable to free globbed filenames.",
-		 function );
-
-		libcerror_error_free(
-		 &error );
-
-		goto on_error;
-	}
-	return( list_object );
-
-on_error:
-	if( filename_string_object != NULL )
-	{
-		Py_DecRef(
-		 filename_string_object );
-	}
-	if( list_object != NULL )
-	{
-		Py_DecRef(
-		 list_object );
-	}
-	if( filenames != NULL )
-	{
-		Py_BEGIN_ALLOW_THREADS
-
-		libsmraw_glob_free(
-		 filenames,
-		 number_of_filenames,
-		 NULL );
-
-		Py_END_ALLOW_THREADS
-	}
-	return( NULL );
-}
-
-#endif /* defined( LIBCSTRING_HAVE_WIDE_SYSTEM_CHARACTER ) */
 
 /* Declarations for DLL import/export
  */
