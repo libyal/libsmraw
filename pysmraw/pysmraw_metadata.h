64dc682d45b002ae6521a16db595a8a6fd07ea19
Joachim Metz <joachim.metz@gmail.com>
2021-01-17 08:26:57 +0100

    Applied updates

--- pysmraw/pysmraw_metadata.h
+++ pysmraw/pysmraw_metadata.h
@@ -1,7 +1,7 @@
 /*
  * Metadata functions for the Python object definition of the libsmraw handle
  *
- * Copyright (C) 2010-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
