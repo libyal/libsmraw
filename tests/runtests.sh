ce55906a1af3d41c4b298980aabe202d6c94f033
Joachim Metz <joachim.metz@gmail.com>
2026-09-17 04:44:04 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libsmraw/.libs/libsmraw.1.dylib && test -f ./pysmraw/.libs/pysmraw.so
+if [ -f "${PWD}/libsmraw/.libs/libsmraw.1.dylib" ] && [ -f ./pysmraw/.libs/pysmraw.so ]
 then
-	install_name_tool -change /usr/local/lib/libsmraw.1.dylib ${PWD}/libsmraw/.libs/libsmraw.1.dylib ./pysmraw/.libs/pysmraw.so
+    install_name_tool -change /usr/local/lib/libsmraw.1.dylib "${PWD}/libsmraw/.libs/libsmraw.1.dylib" ./pysmraw/.libs/pysmraw.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
